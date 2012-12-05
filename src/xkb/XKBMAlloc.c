3c0850900f77bf8a3c9f9486868c29bbf6134b6f
Michal Maruska <mmaruska@gmail.com>
2012-12-05 13:37:27 +0100

    Fix typo: modmap ->  vmodmap.

--- src/xkb/XKBMAlloc.c
+++ src/xkb/XKBMAlloc.c
@@ -697,7 +697,7 @@ XkbChangeKeycodeRange(XkbDescPtr xkb,
                 if (changes) {
                     changes->map.changed = _ExtendRange(changes->map.changed,
                                                 XkbVirtualModMapMask, minKC,
-                                                &changes->map.first_modmap_key,
+                                                &changes->map.first_vmodmap_key,
                                                 &changes->map.num_vmodmap_keys);
                 }
             }
@@ -782,7 +782,7 @@ XkbChangeKeycodeRange(XkbDescPtr xkb,
                 if (changes) {
                     changes->map.changed = _ExtendRange(changes->map.changed,
                                                 XkbVirtualModMapMask, maxKC,
-                                                &changes->map.first_modmap_key,
+                                                &changes->map.first_vmodmap_key,
                                                 &changes->map.num_vmodmap_keys);
                 }
             }
